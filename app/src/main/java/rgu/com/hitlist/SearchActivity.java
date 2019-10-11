39e742e4d687e67736ecf6a1104a156a9f641619
Benjamin_Pcrd <benjamin.picard0@gmail.com>
2019-10-11 18:02:41 +0100

    searchView

---
+++ app/src/main/java/rgu/com/hitlist/SearchActivity.java
@@ -0,0 +1,16 @@
+package rgu.com.hitlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+
+}
