397428be3cc56c65daf2bb1f5eb2f808f77aab17
Benjamin_Pcrd <benjamin.picard0@gmail.com>
2019-10-17 21:29:22 +0100

    back button for the filmDescription activity (multiple parent nav)

--- app/src/main/java/rgu/com/hitlist/FilmDescriptionActivity.java
+++ app/src/main/java/rgu/com/hitlist/FilmDescriptionActivity.java
@@ -1,8 +1,14 @@
 package rgu.com.hitlist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.ActionBar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class FilmDescriptionActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class FilmDescriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_film_description);
+
+        //display the back button
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //destroy the activity and show the one that started it
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
