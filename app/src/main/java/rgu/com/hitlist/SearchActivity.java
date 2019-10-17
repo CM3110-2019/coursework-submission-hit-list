47c16aae76a01aba4453c738b903a8d3e345f801
Benjamin_Pcrd <benjamin.picard0@gmail.com>
2019-10-17 15:54:05 +0100

    recycler view working

--- app/src/main/java/rgu/com/hitlist/SearchActivity.java
+++ app/src/main/java/rgu/com/hitlist/SearchActivity.java
@@ -1,15 +1,43 @@
 package rgu.com.hitlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class SearchActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class SearchActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+
+    MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        // data to populate the RecyclerView with
+        ArrayList<Movie> animalNames = new ArrayList<>();
+        animalNames.add(new Movie("title1", "description1"));
+        animalNames.add(new Movie("title2", "description2"));
+        animalNames.add(new Movie("title3", "description3"));
+        animalNames.add(new Movie("title4", "description4"));
+
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
 
