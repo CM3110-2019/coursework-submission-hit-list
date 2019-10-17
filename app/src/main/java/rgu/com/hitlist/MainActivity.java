819b436f8f4dd6c9100b93ee734ce745c5a40fe0
Benjamin_Pcrd <benjamin.picard0@gmail.com>
2019-10-17 17:45:53 +0100

    header button

--- app/src/main/java/rgu/com/hitlist/MainActivity.java
+++ app/src/main/java/rgu/com/hitlist/MainActivity.java
@@ -1,9 +1,13 @@
 package rgu.com.hitlist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -29,6 +33,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnSearch:
                 startActivity(new Intent(this, SearchActivity.class));
                 break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionSearch:
+                startActivity(new Intent(this, SearchActivity.class));
+                return true;
+            case R.id.actionWatchList:
+                startActivity(new Intent(this, WatchListActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
