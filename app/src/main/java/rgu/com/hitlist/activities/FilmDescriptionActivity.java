57c54a0f897f9d27c0ce887bfc244ac4f051975f
Benjamin_Pcrd <benjamin.picard0@gmail.com>
2019-10-24 11:48:56 +0100

    query of the movie in the filmDescription

--- app/src/main/java/rgu/com/hitlist/activities/FilmDescriptionActivity.java
+++ app/src/main/java/rgu/com/hitlist/activities/FilmDescriptionActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,11 +14,26 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import rgu.com.hitlist.R;
+import rgu.com.hitlist.adapter.MyRecyclerViewAdapter;
 import rgu.com.hitlist.model.Movie;
 import rgu.com.hitlist.tmdbApi.DownloadImageTask;
+import rgu.com.hitlist.tmdbApi.FetchApi;
 
-public class FilmDescriptionActivity extends AppCompatActivity {
+public class FilmDescriptionActivity extends AppCompatActivity implements Response.Listener<String>, Response.ErrorListener {
 
     Movie movie;
 
@@ -32,9 +49,31 @@ public class FilmDescriptionActivity extends AppCompatActivity {
         }
 
         Intent intent = getIntent();
-        movie = (Movie)intent.getSerializableExtra("movie");
-        Log.d("debug", movie.toString());
 
+        if(intent.getSerializableExtra("movie") != null) {
+            movie = (Movie)intent.getSerializableExtra("movie");
+            Log.d("debug", movie.toString());
+            FetchApi.GetMovie(String.valueOf(movie.getId()), this, this, this);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //destroy the activity and show the one that started it
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onResponse(String response) {
+
+        movie = new Gson().fromJson(response, Movie.class);
 
         ImageView ivCover = findViewById(R.id.ivCover);
         TextView tvMovieTitle = findViewById(R.id.tvMovieTitle);
@@ -55,20 +94,27 @@ public class FilmDescriptionActivity extends AppCompatActivity {
         tvMovieVoteAverage.setText(getString(R.string.tvVoteAverage, String.valueOf(movie.getVote_average())));
         tvMovieVoteCount.setText(getString(R.string.tvVoteCount, String.valueOf(movie.getVote_count())));
         tvMovieBudget.setText(getString(R.string.tvBudget, String.valueOf(movie.getBudget())));
-        tvMovieGenre.setText(getString(R.string.tvGenre, movie.getGenres()));
-        tvMovieProdCompanies.setText(getString(R.string.tvProdCompanies, movie.getProduction_companies()));
 
+        String genres = "";
+        Iterator<Map<String, String>> genreIt = movie.getGenres().iterator();
+        while(genreIt.hasNext()) {
+            genres += genreIt.next().get("name");
+            if(genreIt.hasNext()) genres += ", ";
+        }
+        tvMovieGenre.setText(getString(R.string.tvGenre, genres));
+
+        String prodComp = "";
+        Iterator<Map<String, String>> prodCompIt = movie.getProduction_companies().iterator();
+        while(prodCompIt.hasNext()) {
+            prodComp += prodCompIt.next().get("name");
+            if(prodCompIt.hasNext()) prodComp +=", ";
+        }
+        tvMovieProdCompanies.setText(getString(R.string.tvProdCompanies, prodComp));
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                //destroy the activity and show the one that started it
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    public void onErrorResponse(VolleyError error) {
+
     }
+
 }
