42ce888e67ffe736174d98782bc10254360b1fd4
caroberts <callum.roberts@etu.uca.fr>
2019-10-24 11:49:34 +0100

    Created RecyclerView for MainActivity + started on trending movies

--- app/src/main/java/rgu/com/hitlist/activities/MainActivity.java
+++ app/src/main/java/rgu/com/hitlist/activities/MainActivity.java
@@ -7,23 +7,86 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.SearchView;
 
-import rgu.com.hitlist.R;
-import rgu.com.hitlist.database.WatchlistDatabaseHelper;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import rgu.com.hitlist.R;
+import rgu.com.hitlist.adapter.MyRecyclerViewAdapter;
+import rgu.com.hitlist.database.WatchlistDatabaseHelper;
+import rgu.com.hitlist.model.Movie;
+import rgu.com.hitlist.tmdbApi.FetchApi;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Response.ErrorListener{
 
     WatchlistDatabaseHelper userDB;
+    MyRecyclerViewAdapter adapter;
+
+    ArrayList<Movie> trendingMovieData;
+    ArrayList<Movie> trendingTVData;
+    ArrayList<Movie> trendingPersonData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FetchApi.TrendingMoviesDay(this, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+//                    JSONObject jsonResponse = new JSONObject(response);
+//                    JSONArray results = jsonResponse.getJSONArray("results");
+//                    trendingMovieData = new Gson().fromJson(results.toString(), new TypeToken<List<Movie>>(){}.getType());
+                    JSONParser(trendingPersonData, response);
+                }
+                catch (JSONException e){
+                    Log.d("debug", "JSONException: " + e);
+                }
+            }
+        }, this);
+
+        FetchApi.TrendingTVDay(this, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONParser(trendingTVData, response);
+                }
+                catch (JSONException e){
+                    Log.d("debug", "JSONException: " + e);
+                }
+            }
+        }, this);
+
+        FetchApi.TrendingPersonDay(this, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONParser(trendingPersonData, response);
+                }
+                catch (JSONException e){
+                    Log.d("debug", "JSONException: " + e);
+                }
+            }
+        }, this);
 
         //creates the empty database when the MainActivity is created
         userDB = new WatchlistDatabaseHelper(this);
@@ -60,4 +123,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    public void JSONParser(ArrayList<Movie> dataT, String response) throws JSONException {
+
+        JSONObject jsonResponse = new JSONObject(response);
+        JSONArray results = jsonResponse.getJSONArray("results");
+        dataT = new Gson().fromJson(results.toString(), new TypeToken<List<Movie>>(){}.getType());
+
+        RecyclerView recyclerView = findViewById(R.id.rvTrending);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, dataT);
+        adapter.setClickListener((MyRecyclerViewAdapter.ItemClickListener) this);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
 }
--- app/src/main/java/rgu/com/hitlist/tmdbApi/FetchApi.java
+++ app/src/main/java/rgu/com/hitlist/tmdbApi/FetchApi.java
@@ -14,6 +14,9 @@ public class FetchApi {
     private static final String LANGUAGE = "en";
     private static final String SEARCH_MOVIE_URL = "https://api.themoviedb.org/3/search/movie";
     private static final String GET_MOVIE_URL = "https://api.themoviedb.org/3/movie/";
+    private static final String GET_TRENDING_MOVIE_URL = "https://api.themoviedb.org/3/trending/movie/";
+    private static final String GET_TRENDING_TV_URL = "https://api.themoviedb.org/3/trending/tv/";
+    private static final String GET_TRENDING_PERSON_URL = "https://api.themoviedb.org/3/trending/person/";
 
     public static void Search(String query, Context context, Response.Listener<String> res, Response.ErrorListener err) {
         RequestQueue queue = Volley.newRequestQueue(context);
@@ -27,6 +30,33 @@ public class FetchApi {
     public static void GetMovie(String id, Context context, Response.Listener<String> res, Response.ErrorListener err) {
         RequestQueue queue = Volley.newRequestQueue(context);
         String url = GET_MOVIE_URL + id +"?api_key=" + API_KEY + "&language=" + LANGUAGE;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, res, err);
+
+        queue.add(stringRequest);
+    }
+
+    public static void TrendingMoviesDay(Context context, Response.Listener<String> res, Response.ErrorListener err){
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = GET_TRENDING_MOVIE_URL +"/day?api_key=" + API_KEY + "&language=" + LANGUAGE;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, res, err);
+
+        queue.add(stringRequest);
+    }
+
+    public static void TrendingTVDay(Context context, Response.Listener<String> res, Response.ErrorListener err){
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = GET_TRENDING_TV_URL +"/day?api_key=" + API_KEY + "&language=" + LANGUAGE;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, res, err);
+
+        queue.add(stringRequest);
+    }
+
+    public static void TrendingPersonDay(Context context, Response.Listener<String> res, Response.ErrorListener err){
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = GET_TRENDING_PERSON_URL +"/day?api_key=" + API_KEY + "&language=" + LANGUAGE;
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, res, err);
 
