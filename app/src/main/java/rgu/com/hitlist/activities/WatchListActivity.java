c2eace331ea606f48f07f3ae5ab3233a2c792f28
1603682 <g.mitchell9@rgu.ac.uk>
2019-11-14 23:35:26 +0000

    finished db ready to move and work on watchlist recycler view

--- app/src/main/java/rgu/com/hitlist/activities/WatchListActivity.java
+++ app/src/main/java/rgu/com/hitlist/activities/WatchListActivity.java
@@ -1,15 +1,21 @@
 package rgu.com.hitlist.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import rgu.com.hitlist.database.WatchListItem;
+import rgu.com.hitlist.database.WatchlistViewModel;
 import rgu.com.hitlist.model.Movie;
 import rgu.com.hitlist.adapter.MyRecyclerViewAdapter;
 import rgu.com.hitlist.R;
@@ -18,11 +24,26 @@ public class WatchListActivity extends AppCompatActivity implements MyRecyclerVi
 
     MyRecyclerViewAdapter adapter;
 
+    private WatchlistViewModel watchlistViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_list);
+
+        watchlistViewModel = ViewModelProviders.of(this).get(WatchlistViewModel.class);
+        watchlistViewModel.getAllWatchlistItems().observe(this, new Observer<List<WatchListItem>>() {
+            @Override
+            public void onChanged(List<WatchListItem> watchListItems) {
+                //recycler view updater
+                Toast.makeText(WatchListActivity.this, "database created", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         setTitle(R.string.titleWatchList);
+
+
 
         // data to populate the RecyclerView with
         /*List<Movie> data = new ArrayList<>();
--- app/src/main/java/rgu/com/hitlist/database/WatchlistDB.java
+++ app/src/main/java/rgu/com/hitlist/database/WatchlistDB.java
@@ -1,9 +1,13 @@
 package rgu.com.hitlist.database;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 import android.content.Context;
+import android.os.AsyncTask;
 
 @Database(entities = {WatchListItem.class}, version = 1)
 public abstract class WatchlistDB extends RoomDatabase {
@@ -16,8 +20,32 @@ public abstract class WatchlistDB extends RoomDatabase {
         if(instance == null){
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     WatchlistDB.class,  "watchlist_database")
-                    .fallbackToDestructiveMigration().build();
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build();
         }
         return instance;
     }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopAsyncTask extends AsyncTask<Void,Void,Void>{
+        private DAO DAO;
+        private PopAsyncTask(WatchlistDB db){
+            DAO = db.DAO();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            //creating a test to see that i can populate the db
+            DAO.insert(new WatchListItem("1","a","a test","live"));
+            return null;
+        }
+    }
+
 }
